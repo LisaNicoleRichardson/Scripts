d3a44472842b5c5d83237aa999563d1dcca7e8c7
Lisa Nicole Richardson <lisa@ip-172-31-30-211.us-east-2.compute.internal>
2017-11-08 10:26:25 +0000

    adding functionality to creation of aggregated data script

--- Create aggregate data.R
+++ Create aggregate data.R
@@ -12,13 +12,14 @@ library(memisc)
 library(xlsx)
 
 #Set working directory to raw data location
-setwd('C:/Data Accelerator/2015 data/Aggregated data')
+#Raw data is rectangle leve data published annually as part of Sea Fisheries Statistics
+setwd('~/Scripts')
 #Load rectangle data
-rect5 <- read.csv("UK rectangle landings 2016.csv", header=TRUE, sep=",")
-rect4 <- read.csv("UK rectangle landings 2015.csv", header=TRUE, sep=",")
-rect3 <- read.csv("UK rectangle landings 2014.csv", header=TRUE, sep=",")
-rect2 <- read.csv("UK rectangle landings 2013.csv", header=TRUE, sep=",")
-rect1 <- read.csv("UK rectangle landings 2012.csv", header=TRUE, sep=",")
+rect5 <- read.csv("UK rectangle landings 2016.csv", skip=1, header=FALSE, sep=",")
+rect4 <- read.csv("UK rectangle landings 2015.csv", skip=1, header=FALSE, sep=",")
+rect3 <- read.csv("UK rectangle landings 2014.csv", skip=1, header=FALSE, sep=",")
+rect2 <- read.csv("UK rectangle landings 2013.csv", skip=1, header=FALSE, sep=",")
+rect1 <- read.csv("UK rectangle landings 2012.csv", skip=1, header=FALSE, sep=",")
 
 rect <- rbind(rect5, rect4, rect3, rect2, rect1)
 
@@ -30,6 +31,19 @@ colnames(rect)<- c("Year", "Month", "VessNat","FAOArea","ICESDiv","Rect","LG",
 mapData<- aggregate(cbind(rect$LiveWeightT,rect$ValueGBP) , 
           by = list(rect$Year, 
                     rect$Rect, 
-                    rect$SpeciesPub), sum)
+                    rect$SpeciesPub,
+                    rect$LG,
+                    rect$GearCat,
+                    rect$VessNat), sum)
+
+#Create map data
+colnames(mapData)<- c("Year","Rect","Species","Length","Gear","VessNat","QtyT","ValGBP")
+mapData <- as.data.table(mapData)
+mapData <- mapData[!(mapData$Rect =="UNK"),]
+mapData <- mapData[!(mapData$Rect =="00Z5"),]
+mapData <- mapData[!(mapData$Rect =="00Z6"),]
+mapData[, Latitude := ir2d(mapData[,Rect])[1]]
+mapData[, Longitude := ir2d(mapData[,Rect])[2]]
+mapData<- mapData[order(Year),]
 
 write.csv(mapData, "mapData_2012_2016.csv", row.names=FALSE)
